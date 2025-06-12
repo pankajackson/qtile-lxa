7feb4144559c8444d0ebb412f585511d34cb77c2
Pankaj Jackson <pankajackson@live.co.uk>
2025-06-13 03:20:55 +0530

    Added Elasticsearch monitoring Widget

--- src/qtile_lxa/widget/__init__.py
+++ src/qtile_lxa/widget/__init__.py
@@ -1,4 +1,11 @@
 from .DockerCompose import DockerCompose
 from .DockerCompose.typing import DockerComposeConfig
+from .ElasticsearchMonitor import ElasticsearchMonitor
+from .ElasticsearchMonitor.typing import ElasticsearchMonitorConfig
 
-__all__ = ["DockerCompose", "DockerComposeConfig"]
+__all__ = [
+    "DockerCompose",
+    "DockerComposeConfig",
+    "ElasticsearchMonitor",
+    "ElasticsearchMonitorConfig",
+]
