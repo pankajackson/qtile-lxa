664c61ad7cd03a1841a4b2447fcd9ab020ca671d
Pankaj Jackson <pankajackson@live.co.uk>
2025-06-15 22:49:55 +0530

    Added Notification utility

---
+++ src/qtile_lxa/utils/Notification/__init__.py
@@ -0,0 +1,34 @@
+import subprocess
+
+
+def notify_dunst(
+    title: str,
+    msg: str,
+    progress: int | None = None,
+    app_name: str | None = None,
+    app_id: int | None = None,
+    icon: str | None = None,
+    timeout: int | None = None,
+) -> None:
+    cmd = ["dunstify"]
+
+    if app_name:
+        cmd.extend(["-a", app_name])
+
+    if app_id is not None:
+        cmd.extend(["-r", str(app_id)])
+
+    if icon:
+        cmd.extend(["-i", icon])
+
+    if progress is not None:
+        cmd.extend(["-h", f"int:value:{progress}", f"{title}: {progress}%"])
+    else:
+        cmd.append(title)
+
+    cmd.append(msg)
+
+    if timeout is not None:
+        cmd.extend(["-t", str(timeout)])
+
+    subprocess.run(cmd, check=True)
