6eb75a35bd555fda4fda1aede88322ccfaa4e2e0
Pankaj Jackson <pankajackson@live.co.uk>
2025-08-08 23:58:47 +0530

    bugfix

--- src/qtile_lxa/widget/__init__.py
+++ src/qtile_lxa/widget/__init__.py
@@ -13,6 +13,7 @@ from . import (
     url_monitor,
     vagrant,
     widgetbox,
+    multipass,
 )
 
 __all__ = [
@@ -30,4 +31,5 @@ __all__ = [
     "url_monitor",
     "vagrant",
     "widgetbox",
+    "multipass",
 ]
