457b61e31d5c6916a320763cc7b250523de0c17e
Pankaj Jackson <pankajackson@live.co.uk>
2025-11-29 23:46:43 +0530

    removed unnecessary vagrant_dir check

--- src/qtile_lxa/widget/vagrant/typing_vm.py
+++ src/qtile_lxa/widget/vagrant/typing_vm.py
@@ -662,16 +662,3 @@ class VagrantVMConfig:
         # provider_config
         if not isinstance(self.provider_config, dict):
             raise TypeError("provider_config must be dict[str, Any].")
-
-        # vagrant_dir
-        if self.vagrant_dir:
-            if not isinstance(self.vagrant_dir, Path):
-                raise TypeError("vagrant_dir must be Path.")
-            if not self.vagrant_dir.exists():
-                raise FileNotFoundError(
-                    f"Vagrant directory {self.vagrant_dir} does not exist."
-                )
-            if not self.vagrant_dir.is_dir():
-                raise TypeError(
-                    f"Vagrant directory {self.vagrant_dir} is not a directory."
-                )
