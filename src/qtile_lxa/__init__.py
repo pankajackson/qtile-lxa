515a38956b99309b631a7bbf0b50a389687dff2a
Pankaj Jackson <pankajackson@live.co.uk>
2025-12-17 22:23:27 +0530

    updated vidwall cache path

--- src/qtile_lxa/__init__.py
+++ src/qtile_lxa/__init__.py
@@ -28,7 +28,7 @@ class PyWallDefaults:
 @dataclass(frozen=True)
 class VidWallDefaults:
     playlist_path: Path = Path.home() / ".lxa_vidwall_playlists.json"
-    state_cache_path: Path = Path.home() / ".cache/qtile/vidwall.json"
+    state_cache_path: Path = Path.home() / ".cache/qtile/lxa_vidwall_state.json"
 
 
 @dataclass(frozen=True)
